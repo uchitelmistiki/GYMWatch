881540b7d2724b415840501af0040824358d1dca
uchitelmistiki <uchitelmistiki@gmail.com>
2020-12-10 16:24:01 +0400

    init commit spring app

---
+++ src/main/java/ru/um/gymwatch/GymwatchApplication.java
@@ -0,0 +1,13 @@
+package ru.um.gymwatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymwatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymwatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/um/gymwatch/GymwatchApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.um.gymwatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymwatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
