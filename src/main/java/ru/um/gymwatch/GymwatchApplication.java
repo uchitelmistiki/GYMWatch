4e86131f495ce94d2dbaa58eeb98b1f7a880c0e0
uchitelmistiki <uchitelmistiki@gmail.com>
2021-01-05 18:16:36 +0400

    add cors config

--- src/main/java/ru/um/gymwatch/GymwatchApplication.java
+++ src/main/java/ru/um/gymwatch/GymwatchApplication.java
@@ -1,5 +1,6 @@
 package ru.um.gymwatch;
 
+import org.apache.tomcat.util.codec.binary.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import ru.um.gymwatch.utils.DayOfWeekGetter;
@@ -7,9 +8,13 @@ import ru.um.gymwatch.utils.DayOfWeekGetter;
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.text.StringCharacterIterator;
 import java.time.DayOfWeek;
 import java.time.format.TextStyle;
 import java.util.Locale;
+import java.util.StringJoiner;
+import java.util.StringTokenizer;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class GymwatchApplication {
---
+++ src/main/java/ru/um/gymwatch/config/WebConfigurer.java
@@ -0,0 +1,19 @@
+package ru.um.gymwatch.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+class WebConfigurer implements WebMvcConfigurer {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
+}
